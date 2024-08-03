dabaea80c54a8e4b469018eb37ebcac586bcc68b
docimmortal <docimmortal@gmail.com>
2024-08-03 13:01:13 -0400

    Adding  Game over and restart images
    Adding  Game over and restart images

--- src/application/utils/ScreenUtil.java
+++ src/application/utils/ScreenUtil.java
@@ -1,5 +1,22 @@
 package application.utils;
 
+import application.Main;
+import application.fxcomponents.ImageLoader;
+import javafx.scene.image.ImageView;
+
 public class ScreenUtil {
 
+	public static ImageView GameOver() {
+		ImageView imageView = ImageLoader.load("images\\backgrounds\\GameOver.png",false);
+		int x=(int)Main.screenBounds.getWidth()/2;
+		int y=(int)Main.screenBounds.getHeight()/2;
+		
+		// temp
+		x=750-(int)(imageView.getImage().getWidth()/2);
+		y=450-(int)(imageView.getImage().getHeight()/2);
+		
+		imageView.setLayoutX(x);
+		imageView.setLayoutY(y);
+		return imageView;
+	}
 }
